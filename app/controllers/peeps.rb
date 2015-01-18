434ded3d72a3f605819c697cb1ae8ab9c35f5b8e
Danielle Demkiw <ddemkiw@gmail.com>
2015-01-18 14:37:27 +0000

    Adding css styling

--- app/controllers/peeps.rb
+++ app/controllers/peeps.rb
@@ -3,14 +3,10 @@ get '/' do
   erb :index
 end
 
-get '/peeps/new' do
-  erb :"links/new" 
-end
-
 post '/peeps' do
   text = params["text"]
-  Peep.create(:user_id => current_user.id, :text => text, :timestamp => Time.now)
   @username = current_user.username if current_user
+  Peep.create(:user_id => current_user.id, :text => text, :timestamp => Time.now)
   redirect to('/')
 end
 
